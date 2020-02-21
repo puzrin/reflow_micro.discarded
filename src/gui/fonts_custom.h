fd9e10c8f7500e2bfe0ecbe411eae0b32b30ff48
Vitaly Puzrin <vitaly@rcdesign.ru>
2020-02-21 13:27:40 +0300

    more configs

---
+++ src/gui/fonts_custom.h
@@ -0,0 +1,16 @@
+#ifndef __FONTS_CUSTOM__
+#define __FONTS_CUSTOM__
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include "lvgl.h"
+
+LV_FONT_DECLARE(my_font_roboto_14)
+LV_FONT_DECLARE(my_font_roboto_16)
+
+#ifdef __cplusplus
+}
+#endif
+#endif
